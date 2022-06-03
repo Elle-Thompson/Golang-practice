5268fe61ad22cf937b706f031a3ae8a2d4d3ede6
Elle Thompson <louis.thompson@post.com>
2022-06-03 10:41:35 -0700

    Pushing test data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Golang-practice
+
+This is a project used entirely for educational purposes.
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main () {
+	conferenceName := "Go Conference"
+	const conferenceTickets int= 50
+	var remainingTickets uint = 50
+	
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets )
+	fmt.Println("Get your tickets here to attend")
+
+
+
+   var userName string
+   var userTickets int  
+    // ask for their name
+
+   userName = "Tom"
+   userTickets = 2
+   fmt.Println(userName)
+   fmt.Println(userTickets)
+
+
+
+
+}
